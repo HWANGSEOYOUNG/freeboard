cc9ec7b7f5e8508c3a311889b4ff1b3d9de85cb9
SEOYOUNG <seo089@naver.com>
2020-02-03 09:02:13 +0900

    환경설정

---
+++ src/main/java/com/angela/board/config/H2ServerConfig.java
@@ -0,0 +1,19 @@
+package com.angela.board.config;
+
+import org.h2.tools.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.sql.SQLException;
+
+@Configuration
+@Profile("h2")
+public class H2ServerConfig {
+
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Server h2Server() throws SQLException {
+        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
+    }
+
+}
